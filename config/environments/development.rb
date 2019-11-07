25c51cf2ff68bb0fbe28a25da3f529b43d8b2ae3
Nehemiah Mekonnen <your.nmmekonnen@gmail.com>
2019-11-07 17:22:52 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,9 +32,15 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  
+  
   config.action_mailer.perform_caching = false
+  host = 'https://b5879efd3b134780aafc754a49b8fe99.vfs.cloud9.us-east-2.amazonaws.com/' # Don't use this literally; use your local dev host instead
+  # Use this on the cloud IDE.
+  config.action_mailer.default_url_options = { host: host, protocol: 'https://b5879efd3b134780aafc754a49b8fe99.vfs.cloud9.us-east-2.amazonaws.com/' }
+  # Use this if developing on localhost.
+  # config.action_mailer.default_url_options = { host: host, protocol: 'https://b5879efd3b134780aafc754a49b8fe99.vfs.cloud9.us-east-2.amazonaws.com/' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
